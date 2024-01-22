bd790f2ead557693bc79786b9e8ea66f6d86cf4f
Willy Reyno <w.reyno@thecodingmachine.com>
2024-01-22 15:10:19 +0100

    Refactor doctrine commands to use ArrayInput instead of Process

--- src/Command/ResetAppCommand.php
+++ src/Command/ResetAppCommand.php
@@ -6,6 +6,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -28,32 +29,34 @@ class ResetAppCommand extends Command
         }
 
         // Remove existing tables
-        $schemaDrop = new Process(['bin/console', 'doctrine:schema:drop', '--force']);
-        $schemaDrop->mustRun();
-        echo $schemaDrop->getOutput();
+        $schemaDrop = new ArrayInput([
+            'command' => 'doctrine:schema:drop',
+            '--force' => true
+        ]);
+        $schemaDropCode = $this->getApplication()->doRun($schemaDrop, $output);
 
-        if (!$schemaDrop->isSuccessful()) {
-            echo $schemaDrop->getOutput();
+        if ($schemaDropCode !== 0) {
             return Command::FAILURE;
         }
 
         // Recreate tables
-        $schemaCreate = new Process(['bin/console', 'doctrine:schema:create']);
-        $schemaCreate->mustRun();
-        echo $schemaCreate->getOutput();
+        $schemaCreate = new ArrayInput([
+            'command' => 'doctrine:schema:create',
+        ]);
+        $schemaCreateCode = $this->getApplication()->doRun($schemaCreate, $output);
 
-        if (!$schemaCreate->isSuccessful()) {
-            echo $schemaCreate->getOutput();
+        if ($schemaCreateCode !== 0) {
             return Command::FAILURE;
         }
 
         // Load fixtures
-        $loadFixtures = new Process(['bin/console', 'doctrine:fixtures:load', '-n']);
-        $loadFixtures->mustRun();
-        echo $loadFixtures->getOutput();
+        $loadFixtures = new ArrayInput([
+            'command' => 'doctrine:fixtures:load'
+        ]);
+        $loadFixtures->setInteractive(false);
+        $loadFixturesCode = $this->getApplication()->doRun($loadFixtures, $output);
 
-        if (!$loadFixtures->isSuccessful()) {
-            echo $loadFixtures->getOutput();
+        if ($loadFixturesCode !== 0) {
             return Command::FAILURE;
         }
 
