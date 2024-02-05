6c829e13d9db141a8823b995460bc99f6c37a3a1
Willy Reyno <w.reyno@thecodingmachine.com>
2024-02-05 17:41:10 +0100

    Add missing import

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
