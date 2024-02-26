d34b67be66a70a1236197d5ebfb3fb315fc2e8d5
Willy Reyno <w.reyno@thecodingmachine.com>
2024-02-26 17:16:51 +0100

    Remove old upgradePassword method

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -44,20 +44,6 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         }
     }
 
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
-        }
-
-        $user->setPassword($newHashedPassword);
-
-        $this->save($user, true);
-    }
-
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */
